d4d30f18a3c5d5a943f032e433b7ae4a3bb344a7
Jeeshitha <23BQ1A12E9@vvit.net>
2026-06-05 09:49:13 +0530

    Update logger.ts

--- logging_middleware/src/logger.ts
+++ logging_middleware/src/logger.ts
@@ -1,1 +1,13 @@
-
+export async function Log(
+  stack: string,
+  level: string,
+  packageName: string,
+  message: string
+) {
+  console.log({
+    stack,
+    level,
+    package: packageName,
+    message,
+  });
+}
